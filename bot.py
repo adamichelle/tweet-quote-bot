993d61a1b91e866d0b557aa40eedba12876fae27
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-08-27 20:45:27 +0100

    create bot functionality

---
+++ bot.py
@@ -0,0 +1,47 @@
+import json
+import random
+import time
+import sys
+import tweepy
+import credentials
+
+consumer_key = credentials.API_KEY
+consumer_secret_key = credentials.API_SECRET_KEY
+access_token = credentials.ACCESS_TOKEN
+access_token_secret = credentials.ACCESS_TOKEN_SECRET
+
+def get_quotes():
+    with open('data.json') as f:
+        quotes_json = json.load(f)
+    return quotes_json['quotes']
+
+def get_random_quote():
+    quotes = get_quotes()
+    random_quote = random.choice(quotes)
+    return random_quote
+
+def create_tweet():
+    quote = get_random_quote()
+    tweet = """
+            {}
+            ~{}
+            """.format(quote['quote'], quote['character'])
+    return tweet
+
+def tweet_quote():
+    #interval = 60 * 60 * 24
+    interval = 60
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    test_tweet = create_tweet()
+    api.update_status(test_tweet)
+    while True:
+        print('getting a random quote...')        
+        tweet = create_tweet()
+        api.update_status(tweet)
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    tweet_quote()
