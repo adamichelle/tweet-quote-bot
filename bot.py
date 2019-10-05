7a8d20c7f1477ff10182fc0218be10f309d6bae9
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-10-05 17:52:50 +0100

    remove extra tweet code

--- bot.py
+++ bot.py
@@ -30,13 +30,13 @@ def create_tweet():
 
 def tweet_quote():
     interval = 60 * 24 * 24
-    
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    tweet = create_tweet()
-    api.update_status(tweet)
+    # tweet = create_tweet()
+    # api.update_status(tweet)
 
     while True:
         print('getting a random quote...')        
