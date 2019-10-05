52d5055c14702849a1668bd19c30063fa54b1b65
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-10-05 01:59:00 +0100

    Add url to app

--- server.py
+++ server.py
@@ -2,4 +2,9 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Tweeting a GOT Quote..."
+
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
