ef6e983bc1b6d17d1567a119751706442966b77a
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-10-05 02:46:48 +0100

    import bot.py and create tweet in server

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from os import environ
 from flask import Flask
+import bot
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    bot.create_tweet()
     return "Tweeting a GOT Quote..."
 
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
