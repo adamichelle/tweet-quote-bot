1daaf04d866ec01ac7c2bbe26510b203bd72904e
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-10-05 17:56:20 +0100

    interval changed to 24 hours

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def create_tweet():
     return tweet
 
 def tweet_quote():
-    interval = 60 * 24 * 24
+    interval = 60 * 60 * 24
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
